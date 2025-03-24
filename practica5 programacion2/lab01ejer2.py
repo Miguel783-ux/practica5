b4927db6af856b7f6c27e46512ba1f5e48059955
Miguel783-ux <espinozamendozamiguel8@gmail.com>
2025-03-23 21:18:23 -0400

    Actualizar lab01ejer2.py

--- practica5 programacion2/lab01ejer2.py
+++ practica5 programacion2/lab01ejer2.py
@@ -1,24 +1,22 @@
-class Matriz:
-    def _init_(self, datos):
+import math
+
+class Estadisticas:
+    def __init__(self, datos):
         self.datos = datos
-        self.filas = len(datos)
-        self.columnas = len(datos[0]) if self.filas > 0 else 0
-    def _add_(self, otra):
-        if self.filas != otra.filas or self.columnas != otra.columnas:
-            raise ValueError("Las matrices deben tener las mismas dimensiones para sumarse.")
-        resultado = [
-            [self.datos[i][j] + otra.datos[i][j] for j in range(self.columnas)]
-            for i in range(self.filas)
-        ]
-        return Matriz(resultado)
-    def _str_(self):
-        return '\n'.join([' '.join(map(str, fila)) for fila in self.datos])
-m1 = Matriz([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-m2 = Matriz([[9, 8, 7], [6, 5, 4], [3, 2, 1]])
-resultado = m1 + m2
-print("Matriz 1:")
-print(m1)
-print("\nMatriz 2:")
-print(m2)
-print("\nSuma de las matrices:")
-print(resultado)
+
+    def promedio(self):
+        return sum(self.datos) / len(self.datos)
+
+    def desviacion(self):
+        media = self.promedio()
+        suma_cuadrados = sum((x - media) ** 2 for x in self.datos)
+        return math.sqrt(suma_cuadrados / (len(self.datos) - 1))
+numeros = []
+for i in range(10):
+    num = float(input(f"Ingrese el número {i + 1}: "))
+    numeros.append(num)
+estadisticas = Estadisticas(numeros)
+prom = estadisticas.promedio()
+desv = estadisticas.desviacion()
+print(f"\nEl promedio de los números es: {prom:.4f}")
+print(f"La desviación estándar es: {desv:.4f}")
